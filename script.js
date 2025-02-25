a78709af1f31960c16a333a02bdd29dc9b9d6d0d
Daniel Qiu <61307453+yushen-qiu@users.noreply.github.com>
2025-02-25 21:16:21 +1100

    Add hover functionality that colours the cells
    Add EventListener to cells so that when hovering, background colour is
    set to black

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
+
 for (let i = 0; i < 16; i++) {
     const container = document.querySelector(".container");
     const row = document.createElement("div");
     row.style.display = "flex";
     row.style.flex = "1";
+    id = row
     
     container.appendChild(row);
     for (let j = 0; j < 16; j++) {
-        const div = document.createElement("div")
-        row.appendChild(div)
-        div.style.border = "1px solid black";
-        div.style.flex = "1"
+        const cell = document.createElement("div")
+        
+        cell.style.border = "1px solid black";
+        cell.style.flex = "1"
+        cell.class = i
+        cell.addEventListener("mouseover", () => cell.style.backgroundColor = "black")
+        row.appendChild(cell)
     }
 }
