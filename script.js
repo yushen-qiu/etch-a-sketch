4cb05ae9a9cbab9fc2dc2096a682504eab72667e
Daniel Qiu <61307453+yushen-qiu@users.noreply.github.com>
2025-02-26 15:49:06 +1100

    Add button to create new grid
    Move loops into function to create grid of n x n size
    
    Add EventListener to button for click, prompt input and create new grid

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
 
-for (let i = 0; i < 16; i++) {
-    const container = document.querySelector(".container");
-    const row = document.createElement("div");
-    row.style.display = "flex";
-    row.style.flex = "1";
-    id = row
-    
-    container.appendChild(row);
-    for (let j = 0; j < 16; j++) {
-        const cell = document.createElement("div")
+function createGrid(n) {
+    for (let i = 0; i < n; i++) {
+        const container = document.querySelector(".container");
+        const row = document.createElement("div");
+        row.style.display = "flex";
+        row.style.flex = "1";
+        id = row
         
-        cell.style.border = "1px solid black";
-        cell.style.flex = "1"
-        cell.class = i
-        cell.addEventListener("mouseover", () => cell.style.backgroundColor = "black")
-        row.appendChild(cell)
+        container.appendChild(row);
+        for (let j = 0; j < n; j++) {
+            const cell = document.createElement("div")
+            
+            cell.style.border = "0.01em solid black";
+            cell.style.flex = "1"
+            cell.class = i
+            cell.addEventListener("mouseover", () => cell.style.backgroundColor = "black")
+            row.appendChild(cell)
+        }
     }
 }
+
+createGrid(16)
+
+const bttn = document.querySelector("button")
+bttn.addEventListener("click", function() {
+    let input;
+    while (isNaN(input) || input > 100) {
+        input = prompt("Please enter the grid dimensions size you want(Max 100)")
+    }
+    const container = document.querySelector(".container");
+    container.replaceChildren()
+    createGrid(input)
+    console.log(input)
+})
