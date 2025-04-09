c6302e31ef083f82da62abfab437694a58bac7ef
Daniel Qiu <61307453+yushen-qiu@users.noreply.github.com>
2025-04-09 15:57:06 +1000

    Add random color and progressive darkening

--- script.js
+++ script.js
@@ -5,17 +5,28 @@ function createGrid(n) {
         const row = document.createElement("div");
         row.style.display = "flex";
         row.style.flex = "1";
-        id = row
         
         container.appendChild(row);
         for (let j = 0; j < n; j++) {
             const cell = document.createElement("div")
-            
             cell.style.border = "0.01em solid black";
             cell.style.flex = "1"
-            cell.class = i
-            cell.addEventListener("mouseover", () => cell.style.backgroundColor = "black")
             row.appendChild(cell)
+
+            
+            cell.style.flex = "1"
+            cell.addEventListener("mouseover", function() {
+                if (cell.style.backgroundColor == "") {
+                    cell.style.opacity = 0.1
+                    cell.style.backgroundColor = "#" + Math.random().toString(16).substring(2,8)
+                } else {
+                    opacity = parseFloat(cell.style.opacity)
+                    if (opacity < 1) {
+                        cell.style.opacity = opacity + 0.1
+                    }
+                }
+            })
+            
         }
     }
 }
